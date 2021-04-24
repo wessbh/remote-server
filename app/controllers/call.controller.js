dec83dd946abbe7e2fdc52dc4477c98c41790649
wassimbenhamouda <wassimh@360medlink.com>
2021-04-24 22:44:35 +0100

    group call added

--- app/controllers/call.controller.js
+++ app/controllers/call.controller.js
@@ -13,28 +13,38 @@ exports.callSingleUser = (req, res) => {
     console.log("Token is: " + token);
     console.log("Room is: " + roomName);
     sendPushNotification(token, roomName, userName);
-    res.send({message: 'Sending push notification'})
-  };
-  function sendPushNotification(token, roomName, username){
+    res.send({ message: 'Sending push notification' })
+};
+function sendPushNotification(token, roomName, username) {
     var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
         to: token,
-        
+
         notification: {
         },
-        
+
         data: {  //you can send only notification or only data(or include both)
-            title: 'Call', 
-            body: 'You received a call from: '+username,
+            title: 'Call',
+            body: 'You received a call from: ' + username,
             "roomName": roomName
         }
     };
-    fcm.send(message, function(err, response){
+    fcm.send(message, function (err, response) {
         if (err) {
-            console.log("Something has gone wrong!");
+            console.log("Something went wrong!");
+            console.log({error: err});
         } else {
-            console.log('device_token: '+token);
+            console.log('device_token: ' + token);
             console.log("Successfully sent with response: ", response);
         }
     });
-  }
+}
+exports.groupCall = (req, res) => {
+    var userName = req.body.username;
+    var roomName = req.body.roomName;
+    var tokensList = req.body.tokensList;
+    tokensList.forEach(token => {
+    sendPushNotification(token, roomName, userName);
+    });
+    res.status(200).send("Group call sent");
+};
 
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -41,4 +41,9 @@ module.exports = function(app) {
     [authJwt.verifyToken],
     callController.callSingleUser
   );
+  app.post(
+    "/api/call/group",
+    [authJwt.verifyToken],
+    callController.groupCall
+  );
 };
