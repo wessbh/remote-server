534da03bebe52e87c4f3755db5ada8b260fb03f2
wassimbenhamouda <wassimh@360medlink.com>
2021-04-24 01:45:01 +0100

    firebase config

--- app/controllers/call.controller.js
+++ app/controllers/call.controller.js
@@ -1,12 +1,40 @@
 const config = require("../config/auth.config");
+const fireBaseConfig = require("../config/firebase.config");
+
 const db = require("../models");
 const User = db.user;
+var FCM = require('fcm-node');
+var fcm = new FCM(fireBaseConfig.serverKey);
 
 exports.callSingleUser = (req, res) => {
     var token = req.body.token;
     var roomName = req.body.roomName;
     console.log("Token is: " + token);
     console.log("Room is: " + roomName);
+    sendPushNotification(token, roomName, "Wess");
+    res.send({message: 'Sending push notification'})
   };
+  function sendPushNotification(token, roomName, username){
+    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+        to: token,
+        
+        notification: {
+            title: 'Call', 
+            body: 'You received a call from: '+username
+        },
+        
+        data: {  //you can send only notification or only data(or include both)
+            "name": 'wassim',
+            'product': 'PC'
+        }
+    };
+    fcm.send(message, function(err, response){
+        if (err) {
+            console.log("Something has gone wrong!");
+        } else {
+            console.log('device_token: '+token);
+            console.log("Successfully sent with response: ", response);
+        }
+    });
+  }
 
-  
